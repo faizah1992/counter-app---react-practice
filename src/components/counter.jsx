de3f9f1058eea004fffaed1ba70c941f1fadf628
faizah1992 <faizah1992@gmail.com>
2020-09-23 14:36:54 -0500

    handledelete added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,18 +7,20 @@ class Counter extends Component {
 
     handleIncrement = () => {
         // console.log(product)
-        this.setState({count: this.state.count + 1})
+        this.setState( {count: this.state.count + 1 })
     }
    
     render() { 
-
+        console.log(this.props)
         return (
-            <div>
+            <div> 
+
                 <span className={this.getBadgeClasses()}>
                     {this.formatCount()}
                 </span>
                 <button onClick={ () => this.handleIncrement()} className='btn btn-secondary btn-sm'>Increment</button>
-                <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+                 
+                <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
          );
     }
